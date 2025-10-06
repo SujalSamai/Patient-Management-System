0dd429c8c7bc5acdb746ea1221f489604f52fb5c
Sujal Samai <sujalsamai123@gmail.com>
2025-10-06 23:09:00 +0530

    JWT Validation api added

--- auth-service/src/main/java/com/sujalsamai/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/sujalsamai/auth_service/service/AuthService.java
@@ -2,6 +2,9 @@ package com.sujalsamai.auth_service.service;
 
 import com.sujalsamai.auth_service.dto.LoginRequestDTO;
 import com.sujalsamai.auth_service.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,7 @@ import java.util.Optional;
 public class AuthService
 {
 
+   private static final Logger log = LoggerFactory.getLogger(AuthService.class);
    private final UserService userService;
    private final PasswordEncoder passwordEncoder;
    private final JwtUtil jwtUtil;
@@ -28,5 +32,19 @@ public class AuthService
       return userService.findByEmail(requestDTO.getEmail())
                .filter(u -> passwordEncoder.matches(requestDTO.getPassword(), u.getPassword()))
                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+   }
+
+   public boolean validateToken(String token)
+   {
+      try
+      {
+         jwtUtil.validateToken(token);
+         return true;
+      }
+      catch (JwtException e)
+      {
+         log.error("Error while validating token: ", e);
+         return false;
+      }
    }
 }
